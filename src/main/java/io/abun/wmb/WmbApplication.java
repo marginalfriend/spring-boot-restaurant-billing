732e8777288282981ec5a3f481cf376352576cb3
Abdullah <abdullahpsg@gmail.com>
2024-05-08 17:39:01 +0700

    [INITIAL] initial commit

---
+++ src/main/java/io/abun/wmb/WmbApplication.java
@@ -0,0 +1,13 @@
+package io.abun.wmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmbApplication.class, args);
+	}
+
+}
