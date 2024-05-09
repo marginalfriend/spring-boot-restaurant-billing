0a428fe0c0366a5a4fecc49b1eb38dfc7e03a4f8
Abdullah <abdullahpsg@gmail.com>
2024-05-09 11:25:49 +0700

    [NEW] post request for

---
+++ src/main/java/io/abun/wmb/Constants.java
@@ -0,0 +1,8 @@
+package io.abun.wmb;
+
+public class Constants {
+    public static final String VERSION = "v0";
+    public static final String API_URL_ROOT = "api/" + VERSION;
+
+    public static final String CUST_ENDPOINT = "/customers";
+}
--- src/main/java/io/abun/wmb/CustomerManagement/CustomerController.java
+++ src/main/java/io/abun/wmb/CustomerManagement/CustomerController.java
@@ -1,8 +1,17 @@
 package io.abun.wmb.CustomerManagement;
 
+import io.abun.wmb.Constants;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(Constants.API_URL_ROOT + Constants.CUST_ENDPOINT)
 public class CustomerController {
-
+    CustomerService service;
+    @PostMapping
+    public Customer create(Customer customer) {
+        return service.create(customer);
+    }
 }
