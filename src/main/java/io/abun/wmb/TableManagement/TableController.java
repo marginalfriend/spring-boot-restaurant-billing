f34fd1106995ec6468b54e91af83f13710786a91
Abdullah <abdullahpsg@gmail.com>
2024-05-10 18:09:49 +0700

    [NEW] TableController

---
+++ src/main/java/io/abun/wmb/TableManagement/TableController.java
@@ -0,0 +1,46 @@
+package io.abun.wmb.TableManagement;
+
+import io.abun.wmb.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(Constants.API_URL_ROOT + Constants.TABLE_ENDPOINT)
+public class TableController {
+    @Autowired
+    TableService service;
+
+    @GetMapping
+    public List<TableRecord> findAll(
+            @RequestParam(name = "name",    required = false)   String  name,
+            @RequestParam(name = "minCap",  required = false)   Integer minCap,
+            @RequestParam(name = "maxCap",  required = false)   Integer maxCap
+    ) {
+        return service.findAll(
+                new TableCriteria(
+                        name,
+                        minCap,
+                        maxCap
+                )
+        );
+    }
+
+    @PostMapping
+    public TableRecord create(@RequestBody TableRecord table) {
+        return service.create(table);
+    }
+
+    @PutMapping
+    public TableRecord update(@RequestBody TableRecord table) {
+        return service.update(table);
+    }
+
+    @DeleteMapping
+    public String delete(@RequestBody Integer id) {
+        service.removeById(id);
+
+        return "Successfully removed a table";
+    }
+}
