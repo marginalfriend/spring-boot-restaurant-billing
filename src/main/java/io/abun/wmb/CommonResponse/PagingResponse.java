1dc498108b44576a9d7c35d0cbe9fc148034e1a2
Abdullah <abdullahpsg@gmail.com>
2024-05-13 17:33:10 +0700

    [NEW] PagingResponse

--- src/main/java/io/abun/wmb/CommonResponse/PagingResponse.java
+++ src/main/java/io/abun/wmb/CommonResponse/PagingResponse.java
@@ -1,4 +1,15 @@
 package io.abun.wmb.CommonResponse;
+import lombok.*;
 
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PagingResponse {
+    private Integer totalPages;
+    private Long    totalElements;
+    private Integer page;
+    private Integer size;
+    private Boolean hasNext;
+    private Boolean hasPrevious;
 }
