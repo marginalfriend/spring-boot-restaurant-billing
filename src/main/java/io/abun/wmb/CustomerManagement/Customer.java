54c690aa1a7d5600fab30aee28b99ac09754499f
Abdullah <abdullahpsg@gmail.com>
2024-05-14 14:32:44 +0700

    [NEW] constraintviolation exception handler

--- src/main/java/io/abun/wmb/CustomerManagement/Customer.java
+++ src/main/java/io/abun/wmb/CustomerManagement/Customer.java
@@ -1,11 +1,16 @@
 package io.abun.wmb.CustomerManagement;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
 public record Customer(
         UUID    id,
+        @NotBlank(message = "Name is required")
         String  name,
+        @NotBlank(message = "Phone number is required")
         String  phone,
+        @NotBlank(message = "Membership state is required")
         Boolean isMember
 ) {
 }
--- src/main/java/io/abun/wmb/ErrorHandler/ErrorController.java
+++ src/main/java/io/abun/wmb/ErrorHandler/ErrorController.java
@@ -1,5 +1,8 @@
 package io.abun.wmb.ErrorHandler;
 
+import io.abun.wmb.CommonResponse.CommonResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,9 +12,26 @@ import org.springframework.web.server.ResponseStatusException;
 public class ErrorController {
     @ExceptionHandler( {ResponseStatusException.class} )
     public ResponseEntity<?> responseStatusException (ResponseStatusException exception) {
+        CommonResponse<?> response = CommonResponse.builder()
+                .statusCode(exception.getStatusCode().value())
+                .message(exception.getReason())
+                .build();
+
         return ResponseEntity
                 .status(exception.getStatusCode())
-                .body(exception.getReason());
+                .body(response);
     }
 
+    @ExceptionHandler( {ConstraintViolationException.class} )
+    public ResponseEntity<?> constraintViolationException (ConstraintViolationException exception) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
+        CommonResponse<?> response = CommonResponse.builder()
+                .statusCode(badRequest.value())
+                .message(exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(badRequest)
+                .body(response);
+    }
 }
