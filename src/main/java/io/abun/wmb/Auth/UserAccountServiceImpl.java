9dd58c1a27e080df22a91bc376e89c4be95a6f42
Abdullah <abdullahpsg@gmail.com>
2024-05-20 11:32:35 +0700

    [NEW] loadUserById

--- src/main/java/io/abun/wmb/Auth/UserAccountServiceImpl.java
+++ src/main/java/io/abun/wmb/Auth/UserAccountServiceImpl.java
@@ -3,17 +3,32 @@ package io.abun.wmb.Auth;
 import io.abun.wmb.Auth.interfaces.UserAccountRepository;
 import io.abun.wmb.Auth.interfaces.UserAccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserAccountServiceImpl implements UserAccountService {
     final UserAccountRepository userAccountRepository;
+
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userAccountRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("username not found"));
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserAccountEntity loadUserById(UUID id) {
+        return userAccountRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
+        );
     }
 }
--- src/main/java/io/abun/wmb/Auth/interfaces/UserAccountService.java
+++ src/main/java/io/abun/wmb/Auth/interfaces/UserAccountService.java
@@ -1,6 +1,10 @@
 package io.abun.wmb.Auth.interfaces;
 
+import io.abun.wmb.Auth.UserAccountEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.UUID;
+
 public interface UserAccountService extends UserDetailsService {
+    UserAccountEntity loadUserById(UUID id);
 }
